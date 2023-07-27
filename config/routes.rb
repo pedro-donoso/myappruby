4087205f1e1c587e85a51185cad04f8bb84cffcd
pedro-donoso <pedro.donoso.r@usach.cl>
2023-07-27 16:42:20 -0400

    agrego rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/technology'
+  get '/technology' to: 'news#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
